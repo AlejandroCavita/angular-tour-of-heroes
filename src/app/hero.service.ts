8519dbd3ead8d87e197a44411d5632b3824773c5
Alejandro Cavita <alejandrocavita@gmail.com>
2019-08-01 19:41:31 -0500

    First Commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+
+
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor( private messageService: MessageService ) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ ferching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of (HEROES);
+  }
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor( private heroService: HeroService) { }
+
+  heroes: Hero[];
+
+  selectedHero: Hero ;
+
+  onSelect( hero: Hero ) {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    // this.heroes variable do reference to the variable declared above
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
